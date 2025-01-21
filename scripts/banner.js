b0b702b6b3e19f36b7026e47a92fafb2bee8e5cf
IlyaShevchenkoSwimmer <shevchil@mail.ru>
2025-01-22 00:01:14 +0300

    making buy anim

--- scripts/banner.js
+++ scripts/banner.js
@@ -11,4 +11,28 @@ banners.forEach((banner) => {
     banner.classList.remove("banner-card-inactive");
     banner.classList.add("banner-card-active");
   });
+  banner.children[6].addEventListener("click", () => {
+    banner.children[2].animate(
+      [
+        {
+          scale: "1",
+          opacity: "1",
+        },
+        {
+          scale: "0.6",
+          opacity: "0.5",
+          top: "-17dvh",
+        },
+        {
+          scale: "0.2",
+          opacity: "0.1",
+          top: "60dvh",
+        },
+      ],
+      {
+        duration: 1000,
+        iterations: 1,
+      }
+    );
+  });
 });
