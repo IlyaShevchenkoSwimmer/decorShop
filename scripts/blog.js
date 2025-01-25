f06c135832dff7aa480309e6f51b193e8c5b5140
IlyaShevchenkoSwimmer <shevchil@mail.ru>
2025-01-25 19:04:47 +0300

    fixed blog bugs

--- scripts/blog.js
+++ scripts/blog.js
@@ -1,21 +1,18 @@
 const blog = document.querySelector(".blog");
-const bodyWidth = document.querySelector("body").offsetWidth;
+const bodyWidthForBlog = document.querySelector("body").offsetWidth;
 const blogPosition = document.querySelector(".blog-slider-position");
 
-const childWidth = blog.children[0].offsetWidth;
+const childWidthForBlog = blog.children[0].offsetWidth;
 
 blogPosition.style.width = `${Math.round(
-  (bodyWidth /
-    (childWidth * blog.children.length +
-      (bodyWidth / 300) * (blog.children.length - 1))) *
-    100
+  (bodyWidthForBlog / (childWidthForBlog * blog.children.length)) * 100
 )}%`;
 
 blog.addEventListener("scroll", () => {
   blogPosition.style.left = `${Math.round(
     (blog.scrollLeft /
-      (childWidth * blog.children.length +
-        (bodyWidth / 300) * (blog.children.length - 1))) *
+      (childWidthForBlog * blog.children.length +
+        (bodyWidthForBlog / 300) * blog.children.length)) *
       100
   )}%`;
 });
