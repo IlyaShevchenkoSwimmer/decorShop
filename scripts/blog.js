3e7c71bc79b9d16d53e88fe8553728e41082fa63
IlyaShevchenkoSwimmer <shevchil@mail.ru>
2025-01-25 18:29:15 +0300

    made relative proportions

--- scripts/blog.js
+++ scripts/blog.js
@@ -1,26 +1,26 @@
 let currentSlide = 1;
 
-setInterval(() => {
-  const blog = document.querySelector(".blog");
-  const blogSelector = document.querySelectorAll(".scroll-blog");
-  const bodyWidth = document.querySelector("body").offsetWidth;
-  const maxWidth = blog.children[0].offsetWidth * blog.children.length;
-  const howMuchSlides = Math.ceil(maxWidth / bodyWidth);
-  if (currentSlide < howMuchSlides) {
-    blog.style.transform = `translateX(-${currentSlide * bodyWidth}px)`;
-    currentSlide++;
-  } else if (currentSlide === howMuchSlides) {
-    blog.style.transform = `translateX(0px)`;
-    currentSlide = 1;
-  }
-  blogSelector.forEach((selector, index) => {
-    if (index === currentSlide - 1) {
-      selector.className = "scroll-blog active";
-    } else {
-      selector.className = "scroll-blog";
-    }
-  });
-}, 6000);
+// setInterval(() => {
+//   const blog = document.querySelector(".blog");
+//   const blogSelector = document.querySelectorAll(".scroll-blog");
+//   const bodyWidth = document.querySelector("body").offsetWidth;
+//   const maxWidth = blog.children[0].offsetWidth * blog.children.length;
+//   const howMuchSlides = Math.ceil(maxWidth / bodyWidth);
+//   if (currentSlide < howMuchSlides) {
+//     blog.style.transform = `translateX(-${currentSlide * bodyWidth}px)`;
+//     currentSlide++;
+//   } else if (currentSlide === howMuchSlides) {
+//     blog.style.transform = `translateX(0px)`;
+//     currentSlide = 1;
+//   }
+//   blogSelector.forEach((selector, index) => {
+//     if (index === currentSlide - 1) {
+//       selector.className = "scroll-blog active";
+//     } else {
+//       selector.className = "scroll-blog";
+//     }
+//   });
+// }, 6000);
 
 const blog = document.querySelector(".blog");
 const bodyWidth = document.querySelector("body").offsetWidth;
