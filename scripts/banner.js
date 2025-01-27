2e3d25dd105c7c6919c3fabbf29fc4045415a48c
IlyaShevchenkoSwimmer <shevchil@mail.ru>
2025-01-27 15:41:27 +0300

    fixed banners

--- scripts/banner.js
+++ scripts/banner.js
@@ -7,14 +7,16 @@ banners.forEach((banner) => {
       banner.classList.remove("banner-card-inactive");
       banner.classList.remove("banner-card-active");
       banner.classList.add("banner-card-inactive");
+      banner.classList.remove("mobile-banner-card-inactive");
+      banner.classList.remove("mobile-banner-card-active");
+      banner.classList.add("mobile-banner-card-inactive");
     });
     banner.classList.remove("banner-card-inactive");
     banner.classList.add("banner-card-active");
+    banner.classList.remove("mobile-banner-card-inactive");
+    banner.classList.add("mobile-banner-card-active");
   });
   banner.children[6].addEventListener("click", () => {
-    const cart = document
-      .getElementsByClassName("cart-button")[0]
-      .getBoundingClientRect();
     banner.children[2].animate(
       [
         {},
